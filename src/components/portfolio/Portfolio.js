1df180954a6e33a3b544f5cb25a7119f3a588762
Tijani Ahmad <omegabliss23@gmail.com>
2023-02-11 20:03:10 +0100

    Update: removed slide display

--- src/components/portfolio/Portfolio.js
+++ src/components/portfolio/Portfolio.js
@@ -1,8 +1,6 @@
 import { React, useEffect } from "react";
 import "../portfolio/Portfolio.scss";
 import { TiArrowSortedUp } from "react-icons/ti";
-import { Pagination } from "swiper";
-import { Swiper, SwiperSlide } from "swiper/react";
 import Aos from "aos";
 
 // Import Swiper styles
@@ -10,13 +8,45 @@ import "swiper/scss";
 import "swiper/scss/pagination";
 
 const Portfolio = () => {
-
   useEffect(() => {
     Aos.init({
       duration: 1000,
       easing: "ease-in-out",
     });
   }, []);
+
+  const projects = [
+    {
+      id: 1,
+      link: "http://ebryheemxpression.vercel.app",
+      title: "EbryheemXpression",
+      onLinkClick: "View live website",
+    },
+    {
+      id: 2,
+      link: "http://fylofile.netlify.app",
+      title: "Fylo Landing Page",
+      onLinkClick: "View live website",
+    },
+    {
+      id: 3,
+      link: "https://www.behance.net/gallery/153850389/Chase-Fintech-Application",
+      title: "Chase Dashbaord Design",
+      onLinkClick: "View Design",
+    },
+    {
+      id: 4,
+      link: "https://treasurenft.netlify.app/",
+      title: "Treasure NFT Landing Page",
+      onLinkClick: "View live website",
+    },
+    {
+      id: 5,
+      link: "https://sneakers-ecommerce-product.netlify.app/",
+      title: "Sneakers Product Page",
+      onLinkClick: "View live website",
+    },
+  ];
 
   return (
     <section id="portfolio" className="portfolio">
@@ -25,52 +55,23 @@ const Portfolio = () => {
         <h2>10+ projects in Design & Web Development</h2>
       </div>
 
-      <div
-        className="slider__container"
-        data-aos="fade-up"
-        data-aos-once="true"
-      >
-        <Swiper
-          // install Swiper modules
-          modules={[Pagination]}
-          spaceBetween={30}
-          slidesPerView={1}
-          fadeEffect
-          pagination={{ clickable: true }}
-          scrollbar={{ draggable: true }}
-          breakpoints={{
-            425: {
-              width: 415,
-              slidesPerView: 1,
-            },
-            768: {
-              width: 730,
-              slidesPerView: 2,
-            },
-            1024: {
-              width: 1000,
-              slidesPerView: 3,
-            },
-            1440: {
-              width: 1400,
-              slidesPerView: 5,
-            },
-          }}
-        >
-          <SwiperSlide>
+      <div className="projects" data-aos="fade-up" data-aos-once="true">
+        {projects.map((project) => {
+          return (
             <a
-              className="slider__content1"
-              href="http://ebryheemxpression.vercel.app"
+              className="project"
+              href={project.link}
               rel="noreferrer"
               target="_blank"
+              key={project.id}
             >
-              <h2>01</h2>
+              <h2>{project.id}</h2>
 
-              <div className="slider__content1__link">
-                <h2>EbryheemXpression</h2>
+              <div className="project__link">
+                <h2>{project.title}</h2>
 
                 <div className="link__text">
-                  <p>View live website</p>
+                  <p>{project.onLinkClick}</p>
 
                   <div className="card__arrow" id="arrowTail">
                     <TiArrowSortedUp id="arrowHead" />
@@ -78,96 +79,8 @@ const Portfolio = () => {
                 </div>
               </div>
             </a>
-          </SwiperSlide>
-          <SwiperSlide>
-            <a
-              className="slider__content2"
-              href="http://fylofile.netlify.app"
-              rel="noreferrer"
-              target="_blank"
-            >
-              <h2>02</h2>
-
-              <div className="slider__content2__link">
-                <h2>Fylo Landing Page</h2>
-
-                <div className="link__text">
-                  <p>View live website</p>
-
-                  <div className="card__arrow" id="arrowTail">
-                    <TiArrowSortedUp id="arrowHead" />
-                  </div>
-                </div>
-              </div>
-            </a>
-          </SwiperSlide>
-          <SwiperSlide>
-            <a
-              className="slider__content3"
-              href="https://www.behance.net/gallery/153850389/Chase-Fintech-Application"
-              rel="noreferrer"
-              target="_blank"
-            >
-              <h2>03</h2>
-
-              <div className="slider__content3__link">
-                <h2>Chase Dashbaord Design</h2>
-
-                <div className="link__text">
-                  <p>View Design</p>
-
-                  <div className="card__arrow" id="arrowTail">
-                    <TiArrowSortedUp id="arrowHead" />
-                  </div>
-                </div>
-              </div>
-            </a>
-          </SwiperSlide>
-          <SwiperSlide>
-            <a
-              className="slider__content4"
-              href="https://treasurenft.netlify.app/"
-              rel="noreferrer"
-              target="_blank"
-            >
-              <h2>04</h2>
-
-              <div className="slider__content4__link">
-                <h2>Treasure NFT Landing Page</h2>
-
-                <div className="link__text">
-                  <p>View live website</p>
-
-                  <div className="card__arrow" id="arrowTail">
-                    <TiArrowSortedUp id="arrowHead" />
-                  </div>
-                </div>
-              </div>
-            </a>
-          </SwiperSlide>
-          <SwiperSlide>
-            <a
-              className="slider__content5"
-              href="https://sneakers-ecommerce-product.netlify.app/"
-              rel="noreferrer"
-              target="_blank"
-            >
-              <h2>05</h2>
-
-              <div className="slider__content5__link">
-                <h2>Sneakers Product Page</h2>
-
-                <div className="link__text">
-                  <p>View live website</p>
-
-                  <div className="card__arrow" id="arrowTail">
-                    <TiArrowSortedUp id="arrowHead" />
-                  </div>
-                </div>
-              </div>
-            </a>
-          </SwiperSlide>
-        </Swiper>
+          );
+        })}
       </div>
     </section>
   );
